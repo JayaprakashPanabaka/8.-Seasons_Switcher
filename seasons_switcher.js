d19a3ca50a01020b47a04b7c8ebd62e935cd4e73
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2024-02-15 19:16:37 +0530

    first commit

---
+++ seasons_switcher.js
@@ -0,0 +1,24 @@
+let mdImage = document.querySelector('.image-md')
+let smallImage = document.querySelector('.image-small')
+
+
+const spring = () => {
+    mdImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png'
+    smallImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png'
+}
+
+const summer = () => {
+    mdImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png'
+    smallImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png'
+}
+
+const autumn = () => {
+    mdImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png'
+    smallImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png'
+}
+
+const winter = () => {
+    mdImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png'
+    smallImage.src = 'https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png'
+}
+
